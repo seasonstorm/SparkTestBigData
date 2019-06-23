95dfafd624a7585ed297a68c321cf235f07afc8c
seasonstorm <944981730@qq.com>
2019-06-23 15:56:01 +0800

    initpro

---
+++ src/main/java/cn/edu/sparkgroup/hpz/PersonAction.java
@@ -0,0 +1,21 @@
+package cn.edu.sparkgroup.hpz;
+
+import org.springframework.stereotype.Controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+@Controller
+public class PersonAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String execute() throws Exception{
+		return SUCCESS;
+		
+	}
+	
+
+}
